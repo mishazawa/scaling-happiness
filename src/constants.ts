7b44a09fd71a4833c633fac2d21b3ee99aa57b38
asd <macintoshkrug@gmail.com>
2026-08-05 23:12:16 +0300

    change tweens

--- src/constants.ts
+++ src/constants.ts
@@ -25,5 +25,5 @@ export const QUEUE_OFFSET = 0;
 export const GRID_CLUSTER = 2;
 export const LIFES_COUNT = 5;
 
-export const SPAWN_TRANSIT_DURATION = 0.4;
-export const SPAWN_COOLDOWN = 0.3;
+export const SPAWN_TRANSIT_DURATION = 0.1;
+export const SPAWN_COOLDOWN = 0.1;
