6d17bb14f996d3a1abd0a458800354a6b7f28011
asd <macintoshkrug@gmail.com>
2026-08-05 13:39:50 +0300

    added stub for path utils

---
+++ src/utils/path.ts
@@ -0,0 +1,7 @@
+function makePathAroundTheGrid(grid: Grid, padding: number): PathData {
+  // this function should create path around grid with respect to its dimensions.
+}
+
+function DEBUG_pathVisualizer(renderables, scene) {
+  // this function should draw line along the path
+}
